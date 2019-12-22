10745ce100792b08e499bcf7bc5b719f3344a14d
Allyn Alda <allyn.alda@gmail.com>
2019-12-22 13:45:57 +0100

    login local storage

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -7,14 +7,23 @@ import ReactDOM from 'react-dom';
 import Pages from './pages';
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: 'http://localhost:4000/'
-});
 
 const client = new ApolloClient({
-  cache,
-  link
-});
+    cache,
+    link: new HttpLink({
+      uri: 'http://localhost:4000/graphql',
+      headers: {
+        authorization: localStorage.getItem('token'),
+      },
+    }),
+  });
+  
+  cache.writeData({
+    data: {
+      isLoggedIn: !!localStorage.getItem('token'),
+      cartItems: [],
+    },
+  });
 
 ReactDOM.render(
     <ApolloProvider client={client}>
