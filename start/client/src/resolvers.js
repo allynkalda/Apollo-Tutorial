3401944988d30e5cd134610f2bedf1352391a790
Allyn Alda <allyn.alda@gmail.com>
2019-12-26 12:39:58 +0100

    apollo finished

--- start/client/src/resolvers.js
+++ start/client/src/resolvers.js
@@ -21,4 +21,17 @@ export const typeDefs = gql`
   }
 `;
 
-export const resolvers = {};
+export const resolvers = {
+    Mutation: {
+      addOrRemoveFromCart: (_, { id }, { cache }) => {
+        const { cartItems } = cache.readQuery({ query: GET_CART_ITEMS });
+        const data = {
+          cartItems: cartItems.includes(id)
+            ? cartItems.filter(i => i !== id)
+            : [...cartItems, id],
+        };
+        cache.writeQuery({ query: GET_CART_ITEMS, data });
+        return data.cartItems;
+      },
+    },
+  };
