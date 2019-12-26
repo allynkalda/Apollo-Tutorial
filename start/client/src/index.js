42bc4ece9bb646af42996f5e4e0898d18c683a4b
Allyn Alda <allyn.alda@gmail.com>
2019-12-26 12:32:34 +0100

    added cache

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,10 +1,15 @@
 import { ApolloClient } from 'apollo-client';
+import gql from 'graphql-tag';  
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
+import Login from './pages/login';
+import injectStyles from './styles';
+import { resolvers, typeDefs } from './resolvers';
+
 
 const cache = new InMemoryCache();
 
@@ -16,6 +21,8 @@ const client = new ApolloClient({
         authorization: localStorage.getItem('token'),
       },
     }),
+    typeDefs,
+    resolvers,
   });
   
   cache.writeData({
@@ -25,8 +32,22 @@ const client = new ApolloClient({
     },
   });
 
+  const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
+injectStyles();
+
 ReactDOM.render(
     <ApolloProvider client={client}>
-      <Pages />
-    </ApolloProvider>, document.getElementById('root')
+      <IsLoggedIn />
+    </ApolloProvider>,
+    document.getElementById('root'),
   );
